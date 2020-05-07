81b4679342bd43fb95f02d61cdbabb2bb30bb788
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2020-05-07 14:48:55 -0700

    Adds Constants class with supported codecs
    This moves what is a constant out of an inline declaration, to where it should be.

---
+++ DEnc/Constants.cs
@@ -0,0 +1,20 @@
+﻿using DEnc.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEnc
+{
+    public static class Constants
+    {
+        internal static IReadOnlyDictionary<string, Codec> SupportedCodecs { get; } = new Dictionary<string, Codec>()
+        {
+            ["opus"] = new Codec("opus", "ogg", "ogg"),
+            ["aac"] = new Codec("aac", "mp4", "aac"),
+            ["mp3"] = new Codec("mp3", "mp3", "mp3"),
+            ["h264"] = new Codec("h264", "mp4", "mp4"),
+            ["vp8"] = new Codec("vp8", "webm", "webm")
+        };
+    }
+}
