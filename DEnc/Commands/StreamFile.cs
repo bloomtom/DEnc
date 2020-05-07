77593fb5cb8ec730b58fdca9d7469ce81734c505
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2020-05-07 13:50:37 -0700

    Adds Video specific StreamVideoFile
    The existing StreamFile is used in 3 different ways, making it's properties rather ambiguous, which makes it difficult to reason about and extend. In the case of file, my initial findings are that the "Name" property is used to hold the "Bitrate" instead. Thus the new type for clarity.

--- DEnc/Commands/StreamFile.cs
+++ DEnc/Commands/StreamFile.cs
@@ -4,11 +4,28 @@ using System.Text;
 
 namespace DEnc.Commands
 {
-    internal class StreamFile
+    internal interface IStreamFile
+    {
+        StreamType Type { get; set; }
+        int Index { get; set; }
+        string Path { get; set; }
+        string Argument { get; set; }
+    }
+
+    internal class StreamFile : IStreamFile
     {
         public StreamType Type { get; set; }
         public int Index { get; set; }
         public string Name { get; set; }
+        public string Path { get; set; }
+        public string Argument { get; set; }
+    }
+
+    internal class StreamVideoFile : IStreamFile
+    {
+        public StreamType Type { get; set; }
+        public int Index { get; set; }
+        public string Bitrate { get; set; }
         public string Path { get; set; }
         public string Argument { get; set; }
     }
