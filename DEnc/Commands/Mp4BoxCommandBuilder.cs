5eb895ec5944db372e49816a3a3a8b597d6f086d
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2020-05-08 14:08:36 -0700

    Adds Mp4BoxCommand builder and RenderedCommand
    The Mp4Box specific rendered command type is meant to replace the kludged use of CommandBuildResult. Where a single-element StreamFile list was added, and then that element picked out for only it's path.
    
    This resolves the ambiguity and inconsistent use of the previous model at the cost of verboseness.

---
+++ DEnc/Commands/Mp4BoxCommandBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEnc.Commands
+{
+    public static class Mp4BoxCommandBuilder
+    {
+        /// <summary>
+        /// Builds the arguments for the CLI version of Mp4Box.
+        /// </summary>
+        /// <param name="videoFiles">A collection of full paths and roles to input video files to encode from.</param>
+        /// <param name="audioFiles">A collection of full paths and roles to input audio files to encode from.</param>
+        /// <param name="outFilePath">The full path to write the mpd file to.</param>
+        /// <param name="keyInterval">The key interval in milliseconds. This can be derived from: (keyframeInterval / framerate) * 1000</param>
+        /// <param name="additionalFlags">Additional flags to pass to MP4Box. Should include a profile</param>
+        internal static Mp4BoxRenderedCommand BuildMp4boxMpdCommand(
+            IEnumerable<StreamVideoFile> videoFiles, 
+            IEnumerable<StreamAudioFile> audioFiles, 
+            string outFilePath, 
+            int keyInterval, 
+            ICollection<string> additionalFlags)
+        {
+            ICollection<string> flags = additionalFlags ?? new List<string>();
+
+            flags.Add($"-dash {keyInterval}");
+            flags.Add($"-out \"{outFilePath}\"");
+
+            List<string> inputs = new List<string>();
+
+            inputs.AddRange(videoFiles.Select(x => GetInputCommand(x.Bitrate, x.Path)));
+            inputs.AddRange(audioFiles.Select(x => GetInputCommand(x.Name, x.Path)));
+
+            string flagsParams = string.Join("\t", flags);
+            string inputsParams = string.Join("\t", inputs.Select(x => $"\"{x}\""));
+
+            string renderedParams = $"{flagsParams}\t--\t{inputsParams}";
+
+            return new Mp4BoxRenderedCommand(renderedParams, outFilePath);
+        }
+
+        /// <summary>
+        /// Gets the Mp4Box input command for the provided role and path
+        /// </summary>
+        /// <param name="role"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private static string GetInputCommand(string role, string path)
+        {
+            if (!string.IsNullOrWhiteSpace(role))
+            {
+                return $"{path}:role={role}";
+            }
+            return path;
+        }
+    }
+}
---
+++ DEnc/Commands/Mp4BoxRenderedCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEnc.Commands
+{
+    /// <summary>
+    /// Container used to house the rendered Mp4Box command and it's output path
+    /// </summary>
+    internal class Mp4BoxRenderedCommand
+    {
+        public Mp4BoxRenderedCommand(string renderedCommand, string mpdPath)
+        {
+            RenderedCommand = renderedCommand;
+            MpdPath = mpdPath;
+        }
+
+        public string RenderedCommand { get; private set; }
+        public string MpdPath { get; private set; }
+    }
+}
