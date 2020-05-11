5828af6011f75836b878dbc6254d9d0316c7dc6d
Douglas Gaskell <1400380+douglasg14b@users.noreply.github.com>
2020-05-11 12:41:48 -0700

    Adds static RunAndCleanupTest() method to Tests project
    This is meant to replace the TestCleanup() method in the Tests class, so it can be used from more than one place if needed.

---
+++ DEncTests/Utility.cs
@@ -0,0 +1,44 @@
+﻿using DEnc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DEncTests
+{
+    public static class Utility
+    {
+        public static void RunAndCleanupTest(Action<List<DashEncodeResult>> test)
+        {
+            var results = new List<DashEncodeResult>();
+
+            try
+            {
+                test.Invoke(results);
+            }
+            finally
+            {
+                foreach (var s in results)
+                {
+                    if (s?.DashFilePath != null)
+                    {
+                        string basePath = Path.GetDirectoryName(s.DashFilePath);
+                        if (File.Exists(s.DashFilePath))
+                        {
+                            File.Delete(s.DashFilePath);
+                        }
+
+                        foreach (var file in s.MediaFiles)
+                        {
+                            try
+                            {
+                                File.Delete(Path.Combine(basePath, file));
+                            }
+                            catch (Exception) { }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
